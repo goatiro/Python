c136f68cd23118ad57a463405e755c0559a7fc04
Goatiro <87803376+goatiro@users.noreply.github.com>
2021-12-04 01:14:15 +0900

    Add files via upload

--- lottosim.py
+++ lottosim.py
@@ -3,21 +3,7 @@ from tkinter import *
 import requests
 from bs4 import BeautifulSoup
 import random
-# prize
-p1 = 2000000000
-p2 = 55000000
-p3 = 1500000
-p4 = 50000
-p5 = 5000
-# random_number
-rnd = []
 
-while len(rnd) < 6:
-    rnd.append(random.randint(1, 45))
-    a = set(rnd)
-    rnd = list(a)
-
-print(rnd)
 # window_option
 win = Tk()
 
@@ -33,13 +19,13 @@ win.option_add("*Font", "맑은고딕 16")
 
 
 def enter(event):
-    lotto_p()
+    main_func()
 
 # main_lotto_function
 
 
-def lotto_p():
-    n = ent.get()
+def main_func():
+    n = ent2.get()
     url = "https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo={}".format(
         n)
     # div class="win.result"
@@ -51,15 +37,49 @@ def lotto_p():
     global bonus
     num = []
     bonus = []
+    # prize
+    p0 = 0
+    p1 = 2000000000
+    p2 = 55000000
+    p3 = 1500000
+    p4 = 50000
+    p5 = 5000
 
     for i in range(7, 13):
         num.append(int(txt.split("\n")[i]))
 
     bonus.append(int(txt.split("\n")[-4]))
-    same1 = len(set(num) & set(rnd))
-    same2 = len(set(bonus) & set(rnd))
-    print(same1)
-    print(same2)
+
+    for i in range(0, int(ent1.get())):
+        # random_number
+        rnd = []
+        while len(rnd) < 6:
+            rnd.append(random.randint(1, 45))
+            a = set(rnd)
+            rnd = list(a)
+        print(rnd)
+        same1 = len(set(num) & set(rnd))
+        same2 = len(set(bonus) & set(rnd))
+        same3 = same1+same2
+        print(same3)
+        if same1 == 6:
+            p0 = p0+p1
+            print("1st prize")
+        elif same1 == 5 and same2 == 1:
+            p0 = p0+p2
+            print("2nd prize")
+        elif same3 == 5:
+            p0 = p0+p3
+            print("3rd prize")
+        elif same3 == 4:
+            p0 = p0+p4
+            print("4th prize")
+        elif same3 == 3:
+            p0 = p0+p5
+            print("5th prize")
+        else:
+            print("No prize")
+    print("Total prize : ", p0)
 
     lab2.config(text="[당첨번호]")
     lab3.config(text=num)
@@ -67,16 +87,25 @@ def lotto_p():
     lab5.config(text=bonus)
 
 
-lab = Label(win)
-lab.config(text="반복 횟수")
+# Label/Entry/Button
+lab0 = Label(win)
+lab0.config(text="반복 횟수")
+lab0.pack()
 
-ent = Entry(win)
-ent.pack()
+ent1 = Entry(win)
+ent1.pack()
+
+lab1 = Label(win)
+lab1.config(text="복권 회차")
+lab1.pack()
+
+ent2 = Entry(win)
+ent2.pack()
 
 btn = Button(win)
 btn.config(text="입력")
 btn.config(width=20)
-btn.config(command=lotto_p)
+btn.config(command=main_func)
 
 btn.pack()
 
